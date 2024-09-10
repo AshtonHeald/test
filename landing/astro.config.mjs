386d2da9c61d9c705bcba83768c878da4a730ccf
AshTheDev <89755026+AshtonHeald@users.noreply.github.com>
2024-09-09 19:23:01 -0500

    Update astro.config.mjs

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://gh.ashthe.dev/test/',
-  base: '/test'
-});
+export default defineConfig({});
